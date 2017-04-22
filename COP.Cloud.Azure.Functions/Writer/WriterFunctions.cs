1242f34f0ff52ea869242cfe58ffe6eb17e265f0
Maxim Becker <maxim.becker@andrena.de>
2017-04-22 17:16:58 +0200

    implemented writer

--- COP.Cloud.Azure.Functions/Writer/WriterFunctions.cs
+++ COP.Cloud.Azure.Functions/Writer/WriterFunctions.cs
@@ -1,13 +1,30 @@
 ﻿using System.Threading.Tasks;
+using COP.Cloud.Azure.Core.Models;
+using COP.Cloud.Azure.Functions.Core.Models;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace COP.Cloud.Azure.Functions.Writer
 {
     public class WriterFunctions
     {
-        public static async Task ProcessValidatedDataAsync(TraceWriter log)
+        public static async Task WriteValidatedSensorDataAsync([QueueTrigger("validated-sensor-data")] AggregatedSensorData validatedSensorData,
+            [Table("sensordata")] CloudTable sensordatas,
+            TraceWriter log)
         {
-            
+            log.Info($"Saving validated sensor data for sensor {validatedSensorData.SensorId} of type {validatedSensorData.SensorType}.");
+
+            var sensorData = new PersistentSensorData
+            {
+                SensorId = validatedSensorData.SensorId,
+                SensorType = validatedSensorData.SensorType,
+                AggregationType = validatedSensorData.AggregationType,
+                CreatedAt = validatedSensorData.TimeStamp,
+                Value = validatedSensorData.Value
+            };
+            var insertOperation = TableOperation.Insert(sensorData);
+            await sensordatas.ExecuteAsync(insertOperation);
         }
     }
 }
